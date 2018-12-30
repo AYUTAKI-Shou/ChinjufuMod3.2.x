48b4e7735e37892bbcaebd50d42ff9a883af2117
Ayutaki-Shou <ayutaki@shiningsaga.sakura.ne.jp>
2018-12-30 20:37:29 +0900

    Add files via upload

---
+++ 1.12.2/src/main/java/com/ayutaki/chinjufumod/main/Reference.java
@@ -0,0 +1,18 @@
+package com.ayutaki.chinjufumod.main;
+
+public class Reference {
+
+	/* 参照する値をここで管理 Manage referenced values here. */
+	public static final String MOD_ID = "chinjufumod";
+	public static final String MOD_NAME = "ChinjufuMod";
+	public static final String MOD_VERSION = "3.2.2";
+
+	public static final String CLIENT_PROXY_CLASS =
+			"com.ayutaki.chinjufumod.proxy.ClientProxy";
+	public static final String COMMON_PROXY_CLASS =
+			"com.ayutaki.chinjufumod.proxy.CommonProxy";
+
+	public static final String GUI_FACTORY =
+			"com.ayutaki.chinjufumod.config.CMGuiFactory_new";
+
+}
