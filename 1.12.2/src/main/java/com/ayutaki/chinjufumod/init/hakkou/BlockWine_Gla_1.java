0322e950ea08e4c30fa933f3871a22afdaf64a0b
Ayutaki-Shou <ayutaki@shiningsaga.sakura.ne.jp>
2018-12-30 21:35:36 +0900

    Add files via upload

---
+++ 1.12.2/src/main/java/com/ayutaki/chinjufumod/init/hakkou/BlockWine_Gla_1.java
@@ -0,0 +1,70 @@
+package com.ayutaki.chinjufumod.init.hakkou;
+
+import com.ayutaki.chinjufumod.handlers.SoundsHandler;
+import com.ayutaki.chinjufumod.init.ASDecoModHakkou;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public class BlockWine_Gla_1 extends Block {
+
+	public BlockWine_Gla_1() {
+		super(Material.WOOD);
+
+		/*瓶・グラス*/
+		this.setSoundType(SoundType.GLASS);
+		this.setHardness(1.0F);
+		this.setResistance(1.0F);
+	}
+
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing,
+			float hitX, float hitY, float hitZ) {
+
+		ItemStack itemstack = playerIn.getHeldItem(hand);
+
+		if (!itemstack.isEmpty()) {
+			return true;
+		}
+
+		else if (itemstack.isEmpty()) {
+			/* 1秒＝20 酒は ×60=1200 */
+			((EntityLivingBase) playerIn).addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 1200, 0));
+			((EntityLivingBase) playerIn).addPotionEffect(new PotionEffect(MobEffects.SLOWNESS, 1400, 0));
+
+			worldIn.playSound(null, pos, SoundsHandler.GOKU, SoundCategory.PLAYERS, 1.0F, 1.0F);
+			return worldIn.setBlockState(pos, ASDecoModHakkou.WINEGLA_2.getDefaultState());
+		}
+    	return true;
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return new AxisAlignedBB(0.425D, 0.0D, 0.425D, 0.575D, 0.3125D, 0.575D);
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+	    return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+	    return false;
+	}
+
+}
