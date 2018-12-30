4f206585e5304686144f40179258aa9d95036dda
Ayutaki-Shou <ayutaki@shiningsaga.sakura.ne.jp>
2018-12-30 20:33:48 +0900

    Add files via upload

---
+++ 1.12.2/src/main/java/com/ayutaki/chinjufumod/entity/model/ModelFallSakura.java
@@ -0,0 +1,38 @@
+package com.ayutaki.chinjufumod.entity.model;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class ModelFallSakura extends ModelBase {
+
+	public int textureWidth = 16;
+	public int textureHeight = 16;
+	public final ModelRenderer[] box = new ModelRenderer[9];
+
+	public ModelFallSakura() {
+
+	int x = 0;
+	int y = 0;
+
+		for (int i = 0; i < this.box.length; i++) {
+			this.box[i] = new ModelRenderer(this, x, y);
+			this.box[i].addBox(0.0F, -0.1F, 0.0F, 2, 0, 2);
+
+			if (x < 60) {
+				x += 10;
+			}
+			else {
+				x = 0;
+				y += 5;
+			}
+		}
+	}
+
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		for (int i = 0; i < this.box.length; i++) {
+		this.box[i].render(f5);
+		}
+	}
+
+}
